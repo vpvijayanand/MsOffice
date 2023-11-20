92f148ba716745c323f57b93dea5464472a6db7c
Vijayanand Premnath <vpvijayanand@gmail.com>
2023-11-20 07:50:15 +0530

    Create comma_seperated_code.vb

---
+++ excel/multi_select_drop_down_list/comma_seperated_code.vb
@@ -0,0 +1,28 @@
+Private Sub Worksheet_Change(ByVal Target As Range)
+Dim Oldvalue As String
+Dim Newvalue As String
+Application.EnableEvents = True
+On Error GoTo Exitsub
+If Not Intersect(Target, Range("A2")) Is Nothing Then
+  If Target.SpecialCells(xlCellTypeAllValidation) Is Nothing Then
+    GoTo Exitsub
+  Else: If Target.Value = "" Then GoTo Exitsub Else
+    Application.EnableEvents = False
+    Newvalue = Target.Value
+    Application.Undo
+    Oldvalue = Target.Value
+      If Oldvalue = "" Then
+        Target.Value = Newvalue
+      Else
+        If InStr(1, Oldvalue, Newvalue) = 0 Then
+            Target.Value = Oldvalue & ", " & Newvalue
+      Else:
+        Target.Value = Oldvalue
+      End If
+    End If
+  End If
+End If
+Application.EnableEvents = True
+Exitsub:
+Application.EnableEvents = True
+End Sub
